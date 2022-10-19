b0f8253cef0b9c91a7600c50da83c2d691572776
abhijeetsenguptaa <101327881+abhijeetsenguptaa@users.noreply.github.com>
2022-10-19 10:26:53 +0530

    Added Login Details

---
+++ login.js
@@ -0,0 +1,15 @@
+let database_username="abc@gmail.com";
+let database_password=123abc;
+
+let user_name="abc@gmail.com";
+let password=123abc;
+
+if(database_username==user_name){
+ if(database_password==password){
+  console.log("Login Successful");
+}else{
+  console.log("Incorrect Password");
+}
+}else{
+  console.log("Incorrect Credentials");
+}
